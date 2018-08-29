c40f3dfe1e4389164e7b6d32202520fc6e8ae088
Sergei Belonosov <2belonosov@gmail.com>
2018-08-29 09:18:29 +0300

    Initial commit

---
+++ src/main/java/com/mdevi/restbook/RestBookApplication.java
@@ -0,0 +1,12 @@
+package com.mdevi.restbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBookApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mdevi/restbook/domain/Book.java
@@ -0,0 +1,13 @@
+package com.mdevi.restbook.domain;
+
+public class Book {
+
+    private Long id;
+    private String isbn;
+    private String title;
+    private String genre;
+    private String author;
+
+    public Book() {
+    }
+}
